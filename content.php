137a8607032e5aeea7e0c9a70ecc3ff4d6bb84df
AizhanNessipbayeva <aioka_-28@mail.ru>
2017-07-25 16:03:27 +0600

    Delete content.php

--- content.php
+++
@@ -1,7 +0,0 @@
-<div id = "center">
-
-<h1 class="zag">ART organization</h1>
-
-In Canada, a series of artist-run organizations often known as "parallel galleries" or artist-run centres (ARCs) have developed in cities across the country since the 1960s to encourage the exhibition of contemporary works by Canadian and international artists. The purpose is often to provide alternative spaces for exhibition for emerging artists and contemporary artists outside the commercial gallery system. Canadian ARCs usually pay a fee and de-emphasize the selling of work, although this will vary from gallery to gallery. A recommended schedule of payment is provided by the Canadian Artists' Representation (CARFAC), an artists' advocacy group.
-</div>
-</div>
